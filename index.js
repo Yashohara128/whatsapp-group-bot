8cfdfab16f5b20cef7292d5f8733eae6f4fa651c
Yashohara Ganewatta <114895377+Yashohara128@users.noreply.github.com>
2026-09-18 20:50:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const cron = require("node-cron"); // ⏰ වෙලාවට වැඩ කරන
 const TARGET_GROUP_IDS = [
     "120363427144307038@g.us",
     "120363428845309010@g.us",
-    "120363398494943813@g.us"
 ];
 
 const CHROME_PATH = "/usr/bin/chromium-browser";
